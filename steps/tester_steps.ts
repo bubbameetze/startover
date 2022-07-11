4c840f0b2d7f5c3142f926a4093cd79f1e16b645
Bubba Meetze <mmeetze@softdocs.com>
2022-07-11 07:26:26 -0400

    uncommented the stuff I was testing for the single import statement test (didn't work).

--- steps/tester_steps.ts
+++ steps/tester_steps.ts
@@ -1,27 +1,27 @@
-import { Etrieve } from "../steps/xApp"
-// import { browser, protractor } from "protractor";
-// import { generic_class } from "../utils/generic_classes";
-// import { Content } from "../pages/ContentMain_page";
-// import { STS } from "../pages/STS_page";
-// import { Before, Given, Then } from 'cucumber';
+// import { Etrieve } from "../steps/xApp"
+import { browser, protractor } from "protractor";
+import { generic_class } from "../utils/generic_classes";
+import { Content } from "../pages/ContentMain_page";
+import { STS } from "../pages/STS_page";
+import { Before, Given, Then } from 'cucumber';
 
-// var {setDefaultTimeout} = require('cucumber');
-// var {defineSupportCode} = require('cucumber');
+var {setDefaultTimeout} = require('cucumber');
+var {defineSupportCode} = require('cucumber');
 
-// const { Given, When, Then } = require('cucumber');
+const { Given, When, Then } = require('cucumber');
 
-// var fs = require('fs-extra');
-// fs.emptyDir('screenshots/', function (err) {
-//         console.log(err);
-//     });
+var fs = require('fs-extra');
+fs.emptyDir('screenshots/', function (err) {
+        console.log(err);
+    });
 
-// let helperFun: generic_class = new generic_class();
-// let ContentMain: Content = new Content();
-// let STSPage: STS = new STS();
+let helperFun: generic_class = new generic_class();
+let ContentMain: Content = new Content();
+let STSPage: STS = new STS();
 
-// Before({timeout: 60 * 1000}, function() {
-//     setDefaultTimeout(60 * 1000);
-//     });
+Before({timeout: 60 * 1000}, function() {
+    setDefaultTimeout(60 * 1000);
+    });
 
 
 Given('That Jim navigates to {string}', async function (string) {
